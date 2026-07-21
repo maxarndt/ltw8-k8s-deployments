a6f55a1c14d2f83c7da343f0318ea0956030e457
Max Arndt <mail@maxarndt.com>
2026-07-21 11:50:52 +0200

    bump Traefik

--- traefik/install.sh
+++ traefik/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
+TRAEFIK_CHART_VERSION="41.0.2"
+
 echo "Adding Traefik Helm repo..."
 helm repo add traefik https://helm.traefik.io/traefik
 helm repo update
@@ -11,6 +13,7 @@ kubectl create namespace traefik --dry-run=client -o yaml | kubectl apply -f -
 echo "Installing/upgrading Traefik..."
 helm upgrade --install traefik traefik/traefik \
   --namespace traefik \
+  --version "${TRAEFIK_CHART_VERSION}" \
   --values values.yaml \
   --wait
 
